aea60c14bbf23fdda639346ec98b654e842f0cb8
Mary Leddy <maryleddy1@gmail.com>
2020-08-23 10:48:50 -0700

    m

---
+++ greenerkinproduce/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import {NavDropdown, Nav} from 'react-bootstrap'
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+
+</Navbar>
+        );
+    }
+}
+export default Navbar;
